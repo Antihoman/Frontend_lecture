dd6adef8dfcda20359cd90ef6b92eb088d640344
Kirill Lukyanov <kirill.l.2004@yandex.ru>
2023-09-08 19:45:03 +0300

    first commit with ez web

---
+++ index.js
@@ -0,0 +1,7 @@
+//const header = document.querySelector('#header');
+const header = document.getElementById('header');
+header.addEventListener('click', function () {
+    header.style.backgroundColor = 'pink';
+});
+
+console.log('Ky')
